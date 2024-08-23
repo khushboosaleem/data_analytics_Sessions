1ec2f4cefec433afc8f9811886b5e11646fb4dfd
FAATIQ_PC <khushboosaleem25@gmail.com>
2024-08-22 22:57:22 -0700

    Add files via upload

---
+++ session 2.py
@@ -0,0 +1,55 @@
+
+# name = "khus"
+# print(name)
+
+
+# my var = "khus"
+# print(my var)
+
+# my-var = "khu"
+# print(my-var)
+
+# 12my = 12
+# print(12my)
+
+# full_name = "khushboo saleem"
+# print(full_name)
+
+# _full_name = "khush"
+# print(_full_name)
+
+# print('ali's house')
+# print("ali's house")
+
+# print(type("ali's house"))
+
+# name = "ali"
+# print(type(name))
+
+# print(type(123))
+# print(type("123"))
+
+
+
+# print(type(12.33))
+
+"""z = 2+3j
+print(type(z))
+print(z.real)
+print(z.imag)
+
+print(dir(complex))"""
+
+
+# print("""we are learning
+# python""")
+
+# print(3+6)
+# print(3-2)
+# print(3*2)
+# print(3**3)
+# print(5/3)
+# print(5//3)
+# print(round(5/3))
+# print(5%3)
+
